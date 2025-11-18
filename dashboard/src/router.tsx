cbf4f25718164d5dba224d86dd70c811a4f8a00d
Jussi Paalanen <jussi.paalanen@evolver.fi>
2025-11-18 06:54:32 +0200

    WIP

--- dashboard/src/router.tsx
+++ dashboard/src/router.tsx
@@ -4,27 +4,22 @@ import { createBrowserRouter } from "react-router-dom";
 
 import { AppShell } from "./App";
 
-// UUSI:
 import RootIndex, {
     loader as rootIndexLoader,
 } from "./routes/RootIndex";
 
-// Nämä oletan olevan jo olemassa:
 import { OrgLayout } from "./layouts/OrgLayout";
 import AppOverviewPage from "./routes/org/app/AppOverviewPage";
 import OrgOverviewPage from "./routes/org/OrgOverviewPage";
 import SecurityScansIndexRoute, {
     loader as securityScansIndexLoader,
 } from "./routes/org/app/security-scans/SecurityScansIndexRoute";
-import SecurityScansLayout from "./layouts/SecurityScansLayout";
-import SecurityScanOverviewPage from "./routes/org/app/security-scans/SecurityScanOverviewPage";
 import ChannelScansRoute, {
     loader as channelScansLoader,
 } from "./routes/org/app/security-scans/ChannelScansRoute";
 import ChannelScanRunDetailRoute, {
     loader as channelScanRunDetailLoader,
 } from "./routes/org/app/security-scans/ChannelScanRunDetailRoute";
-
 
 // TODO
 const TestReportsPage = () => <div>TODO: Test Reports</div>;
@@ -34,63 +29,51 @@ const NotFoundPage = () => <div>404 – Page not found</div>;
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <AppShell />, // ThemeProvider + CssBaseline + <Outlet />
+        element: <AppShell />,
         children: [
-            // Root path "/"
             {
                 index: true,
                 element: <RootIndex />,
                 loader: rootIndexLoader,
             },
+
             {
                 path: "org/:orgSlug",
-                element: <OrgLayout />,
                 children: [
-                    {
-                        index: true,
-                        element: <OrgOverviewPage />,
-                    },
                     {
                         path: "app/:appSlug",
                         children: [
                             {
-                                index: true,
-                                element: <AppOverviewPage />,
-                            },
-                            {
-                                path: "security-scans",
-                                element: <SecurityScansLayout />,
+                                path: "repo/:repoSlug",
                                 children: [
                                     {
-                                        index: true,
-                                        element: <SecurityScansIndexRoute />,
-                                        loader: securityScansIndexLoader,
+                                        path: "security-scans",
+                                        children: [
+                                            {
+                                                index: true,
+                                                element: <SecurityScansIndexRoute />,
+                                                loader: securityScansIndexLoader,
+                                            },
+                                            {
+                                                path: "channel/:channel",
+                                                element: <ChannelScansRoute />,
+                                                loader: channelScansLoader,
+                                            },
+                                            {
+                                                path: "channel/:channel/run/:timestamp",
+                                                element: <ChannelScanRunDetailRoute />,
+                                                loader: channelScanRunDetailLoader,
+                                            },
+                                        ],
                                     },
-                                    {
-                                        path: "channel/:channel",
-                                        loader: channelScansLoader,
-                                        element: <ChannelScansRoute />,
-                                    },
-                                    {
-                                        path: "channel/:channel/run/:timestamp",
-                                        loader: channelScanRunDetailLoader,
-                                        element: <ChannelScanRunDetailRoute />,
-                                    },
+                                    // TODO add later test-reports, cloud-findings etc.
                                 ],
                             },
-                            {
-                                path: "test-reports",
-                                element: <TestReportsPage />,
-                            },
-                            {
-                                path: "cloud-findings",
-                                element: <CloudFindingsPage />,
-                            },
-                            // TODO repo/:repoSlug routing
                         ],
                     },
                 ],
             },
+
             {
                 path: "*",
                 element: <NotFoundPage />,
